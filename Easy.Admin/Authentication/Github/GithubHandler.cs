4e99c98ac7dc68b5c51d19438e1d04e33fc42764
xxred <xxred0324@qq.com>
2019-03-08 20:28:09 +0800

    添加第三方登录github

---
+++ Easy.Admin/Authentication/Github/GithubHandler.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Easy.Admin.Authentication.Github
+{
+    public class GithubHandler : OAuthHandler<GithubOptions>
+    {
+        public GithubHandler(IOptionsMonitor<GithubOptions> options, ILoggerFactory logger,
+            UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticationTicket> CreateTicketAsync(ClaimsIdentity identity,
+            AuthenticationProperties properties, OAuthTokenResponse tokens)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, Options.UserInformationEndpoint);
+            request.Headers.Authorization = new AuthenticationHeaderValue("", tokens.AccessToken);
+
+            var response = await Backchannel.SendAsync(request, Context.RequestAborted);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"An error occurred when retrieving Github user information ({response.StatusCode}). Please check if the authentication information is correct and the corresponding Github API is enabled.");
+            }
+
+            var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
+            var context = new OAuthCreatingTicketContext(new ClaimsPrincipal(identity), properties, Context, Scheme,
+                Options, Backchannel, tokens, payload);
+
+            context.RunClaimActions();
+            await Events.CreatingTicket(context);
+
+            return new AuthenticationTicket(context.Principal, Scheme.Name);
+        }
+    }
+}
---
+++ Easy.Admin/Authentication/Github/GithubOptions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Easy.Admin.Authentication.Github
+{
+    public class GithubOptions : OAuthOptions
+    {
+        public GithubOptions()
+        {
+            CallbackPath = new PathString("/sign-github");
+            AuthorizationEndpoint = GithubDefaults.AuthorizationEndpoint;
+            TokenEndpoint = GithubDefaults.TokenEndpoint;
+            UserInformationEndpoint = GithubDefaults.UserInformationEndpoint;
+
+            Scope.Add("openid");
+
+            ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier,"id");
+            ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+
+        }
+    }
+}
