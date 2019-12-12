51e20f1fce0939faccc0a7d1a1adfbb1bd1268b9
xxred <xxred0324@qq.com>
2019-12-12 17:28:15 +0800

    完善spa部分代码

--- Easy.Admin/Startup.cs
+++ Easy.Admin/Startup.cs
@@ -76,9 +76,11 @@ namespace Easy.Admin
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            var env = Environment;
+
             app.UseApiExceptionHandler();
 
-            if (Environment.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -129,44 +131,40 @@ namespace Easy.Admin
                 endpoints.MapControllers();
             });
 
-            // 以下为SPA准备
-            if (Environment.WebRootPath != null)
-            {
+            // 如果是开发环境，并且配置了前端源码路径，则启用前端开发中间件
+            // 否则设置打包后的静态文件
+            // ClientAppSourcePath配置优先，生产部署无需配置，因此，改配置放在appsettings.Development.json即可
 
+            var clientAppSourcePath = Configuration["ClientAppSourcePath"];
+
+            if (env.IsDevelopment() && !clientAppSourcePath.IsNullOrWhiteSpace())
+            {
+                app.UseSpa(options =>
+                {
+                    // 开发时，当前目录就是项目目录，而不是bin目录
+                    options.Options.SourcePath = clientAppSourcePath;
+                    //options.Options.StartupTimeout = TimeSpan.FromSeconds(20);
+                    options.UseVueDevelopmentServer("yarn", "start");
+                });
+            }
+            else if (env.WebRootPath != null)
+            {
+                // 如果dist文件夹不存在，说明没有部署前端文件
+                var dist = Path.Combine(env.WebRootPath, "dist");
+                if (!Directory.Exists(dist))
+                {
+                    return;
+                }
 
                 app.UseSpa(options =>
                 {
-                    if (Environment.IsDevelopment())
+                    var staticFileOptions = new StaticFileOptions()
                     {
-                        // 如果前端源码路径配置为空，说明不需要代理服务
-                        var clientAppSourcePath = Configuration["ClientAppSourcePath"];
-                        if (clientAppSourcePath.IsNullOrWhiteSpace())
-                        {
-                            return;
-                        }
+                        FileProvider = new PhysicalFileProvider(dist)
+                    };
 
-                        // 开发时，当前目录就是项目目录，而不是bin目录
-                        options.Options.SourcePath = clientAppSourcePath;
-                        //options.Options.StartupTimeout = TimeSpan.FromSeconds(20);
-                        options.UseVueDevelopmentServer("yarn", "start");
-                    }
-                    else
-                    {
-                        // 如果dist文件夹不存在，说明没有部署前端文件
-                        var dist = Path.Combine(Environment.WebRootPath, "dist");
-                        if (!Directory.Exists(dist))
-                        {
-                            return;
-                        }
-
-                        var staticFileOptions = new StaticFileOptions()
-                        {
-                            FileProvider = new PhysicalFileProvider(dist)
-                        };
-
-                        app.UseSpaStaticFiles(staticFileOptions);
-                        options.Options.DefaultPageStaticFileOptions = staticFileOptions;
-                    }
+                    app.UseSpaStaticFiles(staticFileOptions);
+                    options.Options.DefaultPageStaticFileOptions = staticFileOptions;
                 });
             }
         }
