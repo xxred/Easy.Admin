f989e9ad8dd9c986f40fb8ed004e0ae6990adaba
xxred <xxred0324@qq.com>
2020-07-13 17:57:57 +0800

    添加多语言相关控制器，可后台配置

---
+++ Easy.Admin/Areas/Admin/Controllers/LanguagesController.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using Easy.Admin.Localization.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Easy.Admin.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// 语言
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [DisplayName("语言")]
+    public class LanguagesController : EntityController<Languages>
+    {
+    }
+}
---
+++ Easy.Admin/Areas/Admin/Controllers/LocalizationRecordsController.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using Easy.Admin.Localization.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Easy.Admin.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// 多语言记录
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [DisplayName("多语言记录")]
+    public class LocalizationRecordsController : EntityController<LocalizationRecords>
+    {
+    }
+}
